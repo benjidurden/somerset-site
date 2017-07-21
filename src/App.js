b7b73c6c8e040eddcfc7911b6ae3b9176bce7cc9
benjidurden <benjidurdenn@gmail.com>
2017-07-21 14:28:48 -0400

    navlinks rendered, work on transitions

--- src/App.js
+++ src/App.js
@@ -10,13 +10,14 @@ import Contact from './Contact.js';
 import Sketchbook from './Sketchbook.js';
 import Gallery from './Gallery.js';
 
-let renderNav = () => {
+
+let RenderNav = () => {
  return (
    <div>
    <NavLink to = '/'>Home</NavLink>
-   <NavLink to = '/about/'>About</NavLink>
-   <NavLink to = '/theatre/'>Theatre</NavLink>
-   <NavLink to = '/contact/'>Contact</NavLink>
+   <NavLink to = '/About/'>About</NavLink>
+   <NavLink to = '/Theatre/'>Theatre</NavLink>
+   <NavLink to = '/Contact/'>Contact</NavLink>
    <i className="fa fa-bars fa-2x" id="bars"></i>
    </div>
  )
@@ -34,15 +35,15 @@ class App extends Component {
       <Router>
        <div id="barba-wrapper">
         <div className="barba-container">
-        <renderNav />
+        <RenderNav />
         <i className="fa fa-bars fa-2x" id="bars"></i>
          <Switch>
            <Route exact path = '/' component={Home} />
-           <Route exact path = '/about/' component={About} />
-           <Route exact path = '/contact/' component={Contact} />
-           <Route exact path = '/theatre/' component = {Theatre} />
-           <Route exact path = '/sketchbook/' component={Sketchbook} />
-            <Route path = '/gallery/' component={Gallery} />
+           <Route exact path = '/About/' component={About} />
+           <Route exact path = '/Contact/' component={Contact} />
+           <Route exact path = '/Theatre/' component = {Theatre} />
+           <Route exact path = '/Sketchbook/' component={Sketchbook} />
+            <Route path = '/Gallery/' component={Gallery} />
          </Switch>
          <img src={logo} className="somersetLogo" alt="somersetLogo" />
          </div>
